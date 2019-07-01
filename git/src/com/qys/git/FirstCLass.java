69dc1cafdd9401bbf2c98669946a1159afbff81c
guanghua.pan 潘光华 <guanghua.pan@qiyuesuo.com>
2019-07-01 15:01:48 +0800

    本地工程添加到git

--- git/src/com/qys/git/FirstCLass.java
+++ git/src/com/qys/git/FirstCLass.java
@@ -3,5 +3,12 @@ package com.qys.git;
 public class FirstCLass {
     public static void main(String[] args) {
         System.out.println("My First Class");
+        System.out.println("My First Class");
+        System.out.println("My First Class");
+    }
+
+    public void test(){
+        int a = 10;
+        System.out.println(a+=10);
     }
 }
